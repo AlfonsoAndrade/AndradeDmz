9625eb1052ddb770055dd5de7a57a9bc647898d9
AlfonsoAndrade <andrajesus2015@gmail.com>
2020-10-25 14:07:24 -0500

    Tarea login

--- Login/src/main/java/com/yisus/login/LoginInterfaz.java
+++ Login/src/main/java/com/yisus/login/LoginInterfaz.java
@@ -2,7 +2,8 @@
 package com.yisus.login;
 
 import javax.swing.JOptionPane;
-
+import java.util.ArrayList;
+import java.awt.Color;
 /**
  *
  * @author Sammy Guergachi <sguergachi at gmail.com>
@@ -12,6 +13,7 @@ public class LoginInterfaz extends javax.swing.JFrame {
     public LoginInterfaz() {
         initComponents();
         this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.ORANGE);
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -50,21 +52,34 @@ public class LoginInterfaz extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
+                .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jTextField1)
-                        .addComponent(jTextField2)
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(17, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(1, 1, 1)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(25, 25, 25)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(28, 28, 28))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(39, 39, 39)))))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(12, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -76,12 +91,16 @@ public class LoginInterfaz extends javax.swing.JFrame {
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2)
-                .addContainerGap())
+                .addContainerGap(15, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    //Declaracion de ArrayList
+    ArrayList<String> Usuarios;
+    ArrayList<String> Passwords;
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         String Nombre;
         String Contraseña;
@@ -89,22 +108,48 @@ public class LoginInterfaz extends javax.swing.JFrame {
         Nombre=jTextField1.getText();
         Contraseña=new String(jTextField2.getText());
         
-        if(Nombre.equals("JesusAlfonso")&& Contraseña.equals("Alfonso1234"))
+        //Instancia de ArrayList Usuarios
+        Usuarios = new ArrayList<String>();       
+        //Agregamos  elementos al ArrayList
+        Usuarios.add("JesusAlfonso");
+        Usuarios.add("PedroHernandez");
+        Usuarios.add("PatriciaSaldaña");
+        Usuarios.add("JordanAleman");
+        Usuarios.add("YessicaElizabeth");
+       
+        //Instancia de ArrayList Passwords 
+        Passwords= new ArrayList<String>();
+        //Agregamos  elemenotos al ArrayList
+        Passwords.add("jesus123");
+        Passwords.add("pedroH");
+        Passwords.add("patysal2");
+        Passwords.add("jordan190");
+        Passwords.add("yessi456");
+       
+        //Usamos variables de tipo booleanas para poder comparar datos
+        boolean Usu,Contra;
+        Usu=Usuarios.contains(Nombre);
+        Contra=Passwords.contains(Contraseña);
+        
+        if(Usu== true && Contra==true)
         {
-            JOptionPane.showMessageDialog(null, "¡Ingreso exitoso!");
+            JOptionPane.showMessageDialog(null, "¡Login Correcto");
+            jTextField1.setText("");
+            jTextField2.setText("");
+            jTextField1.requestFocus();
         }
         else
         {
-            JOptionPane.showMessageDialog(null, "Datos ingresados incorrectos!!");
+            JOptionPane.showMessageDialog(null, "Login Incorrecto");
             jTextField1.setText("");
             jTextField2.setText("");
-        }
-        
+            jTextField1.requestFocus();
+        }    
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         
-         int s = JOptionPane.showConfirmDialog(null, "¿Seguro que quieres salir?", "Advertencia", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+         int s = JOptionPane.showConfirmDialog(null, "¿Seguro que quieres cancelar esta acción?", "Advertencia", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
         if(s==JOptionPane.YES_NO_OPTION)
         {
             System.exit(0);
